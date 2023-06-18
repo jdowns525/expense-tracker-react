ffdeb23a04c414bf503e44538d4b884d45701abb
Josseph Downs <downsjosseph@gmail.com>
2023-06-18 00:15:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const ExpenseTracker = () => {
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [transactions, setTransactions] = useState([]);
+  
